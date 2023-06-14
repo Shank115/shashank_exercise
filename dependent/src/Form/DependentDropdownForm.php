27064787a047f103e75acf885b54f4763f7d7c87
Shank115 <i.shashnks@gmail.com>
2023-06-14 14:09:16 +0530

    added

--- dependent/src/Form/DependentDropdownForm.php
+++ dependent/src/Form/DependentDropdownForm.php
@@ -1,90 +1,68 @@
 <?php
 
-namespace Drupal\dependent\Form;
-
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Database\Database;
 
 /**
- * Define a class.
+ * Implements the example form.
  */
-class DependentDropdownForm extends FormBase {
+class MyModuleExampleForm extends FormBase {
 
   /**
    * {@inheritdoc}
    */
   public function getFormId() {
-    return 'dependent_dropdown_Form';
+    return 'my_module_example_form';
   }
 
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    // Location stored in opt variable.
-    $opt = $this->location();
-    // Using getvalue to get the value of category and store it in cat.
-    $cat = $form_state->getValue('category') ?: 'none';
-    // Using getvalue to get the value of available items.
-    $avai = $form_state->getValue('availableitems') ?: 'none';
-    $form['category'] = [
-    // Type is select.
+    $form['country'] = [
       '#type' => 'select',
-    // Providing the title.
-      '#title' => 'country',
-    // Options stored in opt variable.
-      '#options' => $opt,
-    // Default value.
-      'default_value' => $cat,
+      '#title' => $this->t('Country'),
+      '#options' => $this->getCountryOptions(),
+      '#empty_option' => $this->t('- Select -'),
       '#ajax' => [
-    // Event.
-        'callback' => '::DropdownCallback',
-    // This element will get altered.
-        'wrapper' => 'field-container',
-    // Event is change.
+        'callback' => [$this, 'ajaxStateDropdownCallback'],
+        'wrapper' => 'state-dropdown-wrapper',
         'event' => 'change',
+        'progress' => [
+          'type' => 'throbber',
+          'message' => $this->t('Loading...'),
+        ],
       ],
     ];
-    $form['availableitems'] = [
-    // Type is select.
+
+    $form['state'] = [
       '#type' => 'select',
-    // Providing the title.
-      '#title' => 'state',
-    // Options stored in cat variable.
-      '#options' => static::availableItems($cat),
-    // F the form is empty get value from the available items.
-      '#default_value' => !empty($form_state->getValue('availableitems')) ? $form_state->getValue('availableitems') : 'none',
-    // Providing a prefix.
-      '#prefix' => '<div id="field-container"',
-    // Providing suffix.
+      '#title' => $this->t('State'),
+      '#prefix' => '<div id="state-dropdown-wrapper">',
       '#suffix' => '</div>',
+      '#empty_option' => $this->t('- Select -'),
+      '#disabled' => TRUE,
       '#ajax' => [
-    // This is event.
-        'callback' => '::DropdownCallback',
-    // Defining which element will get altered.
-        'wrapper' => 'dist-container',
-    // Event is change.
+        'callback' => [$this, 'ajaxDistrictDropdownCallback'],
+        'wrapper' => 'district-dropdown-wrapper',
         'event' => 'change',
+        'progress' => [
+          'type' => 'throbber',
+          'message' => $this->t('Loading...'),
+        ],
       ],
     ];
+
     $form['district'] = [
-    // Type is select.
       '#type' => 'select',
-    // Providing the title.
-      '#title' => 'district',
-    // Options stored in avai variable.
-      '#options' => static::district($avai),
-    // F the form is empty get value from the available items.
-      '#default_value' => !empty($form_state->getValue('district')) ? $form_state->getValue('district') : '',
-    // Providing a prefix.
-      '#prefix' => '<div id="dist-container"',
-    // Providing suffix.
+      '#title' => $this->t('District'),
+      '#prefix' => '<div id="district-dropdown-wrapper">',
       '#suffix' => '</div>',
+      '#empty_option' => $this->t('- Select -'),
+      '#disabled' => TRUE,
     ];
-    $form['submit'] = [
-      '#type' => 'submit',
-      '#value' => 'Submit',
-    ];
+
     return $form;
   }
 
@@ -92,86 +70,39 @@ class DependentDropdownForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    // Submit form this string is triggered and element stored on trigger.
-    $trigger = (string) $form_state->getTriggeringElement()['#value'];
-    // If it is not equal to submit.
-    if ($trigger != 'submit') {
-      // Rebuild the form state.
-      $form_state->setRebuild();
-    }
+    // Handle form submission if needed.
   }
 
   /**
-   * Function dropdowncallback.
+   * Ajax callback for the state dropdown.
    */
-  public function dropdownCallback(array &$form, FormStateInterface $form_state) {
-    $triggering_element = $form_state->getTriggeringElement();
-    $triggering_element_name = $triggering_element['#name'];
-
-    // If element name is equal to category.
-    if ($triggering_element_name === 'category') {
-      // Return the values of available items.
-      return $form['availableitems'];
-    }
-    // If element name is equal to available items.
-    elseif ($triggering_element_name === 'availableitems') {
-      // Return the district values.
-      return $form['district'];
-    }
-
+  public function ajaxStateDropdownCallback(array &$form, FormStateInterface $form_state) {
+    return $form['state'];
   }
 
   /**
-   * Function is called.
+   * Ajax callback for the district dropdown.
    */
-  public function location() {
-    // Setting the location values.
-    return [
-      'none' => '-none-',
-      'india' => 'india',
-    ];
+  public function ajaxDistrictDropdownCallback(array &$form, FormStateInterface $form_state) {
+    return $form['district'];
   }
 
   /**
-   * Function is called.
+   * Helper function to retrieve country options.
    */
-  public function availableItems($cat) {
-    switch ($cat) {
-      case 'india':
-        $opt = [
-          'karnataka' => 'karnataka',
-          'TN' => 'TN',
-        ];
-        break;
+  private function getCountryOptions() {
+    $query = Database::getConnection()->select('country', 'c');
+    $query->fields('c', ['id', 'name']);
+    $result = $query->execute();
+    $options = [];
 
-      default:
-        $opt = ['none' => '-none-'];
-        break;
+    foreach ($result as $row) {
+      $options[$row->id] = $row->name;
     }
-    return $opt;
+
+    return $options;
   }
 
-  /**
-   * Function is called.
-   */
-  public function district($avai) {
-    switch ($avai) {
-      case 'karnataka':
-        $opt = [
-          'mysore' => 'mysore',
-          'bangalore' => 'bangalore',
-          'mangalore' => 'mangalore',
-        ];
-        break;
-
-      case 'TN':
-        $opt = [
-          'chennai' => 'chennai',
-          'madurai' => 'madurai',
-        ];
-        break;
-    }
-    return $opt;
-  }
+  // Add helper functions to retrieve state and district options based on the selected country.
 
 }
